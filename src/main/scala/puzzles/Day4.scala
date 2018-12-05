0bfaa48a4dcca696b04e4762daf3e573cdfb4214
rinfiyks <tom.gould.92@gmail.com>
2018-12-05 08:29:45 +0000

    temp

---
+++ src/main/scala/puzzles/Day4.scala
@@ -0,0 +1,14 @@
+package puzzles
+
+object Day4 extends Puzzle with App {
+
+  override val day = 4
+
+  case class Guard(id: Int, asleep: Vector[Int])
+
+  input.sorted.foreach(println)
+
+  def asd = {
+
+  }
+}
